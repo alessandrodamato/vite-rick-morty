2ed36dc5cf86350a9574cd41beebe7d2585e0b09
alessandrodamato <alessandro042004@gmail.com>
2024-03-25 15:56:37 +0100

    [add] header markup

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,8 +8,23 @@
 
 <template>
 
-  <header>
-    h
+  <header class="p-5">
+
+    <h1 class="text-center mb-5">- Rick&Morty -</h1>
+
+    <nav class="d-flex justify-content-center">
+
+      <input type="text" class="form-control mx-2" placeholder="Search character">
+
+      <select class="form-select mx-2">
+        <option value="0">Select Status...</option>
+      </select>
+
+      <button class="btn btn-primary mx-2">Search</button>
+      <button class="btn btn-warning mx-2">Reset</button>
+
+    </nav>
+
   </header>
   
 </template>
@@ -17,5 +32,14 @@
 
 
 <style lang="scss" scoped>
+
+  header{
+    input{
+      width: 300px;
+    }
+    select{
+      width: 150px;
+    }
+  }
 
 </style>
